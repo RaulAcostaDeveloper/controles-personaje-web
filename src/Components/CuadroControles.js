cea60feb6d2ce323c76c42939db04c3d78a1ff39
Raúl <raulacostadeveloper@outlook.com>
2023-08-11 16:25:20 -0600

    Primer commit

---
+++ src/Components/CuadroControles.js
@@ -0,0 +1,124 @@
+import '../Styles/CuadroControles.css';
+import Personaje from './Personaje';
+import Selectores from './Selectores';
+import Tecla from './Tecla';
+import { useEffect, useState } from 'react';
+const CuadroControles = () => {
+    const [isSActive, setIsSActive] = useState(false);
+    const [isAActive, setIsLeftArrowActive] = useState(false);
+    const [isWActive, setIsWActive] = useState(false);
+    const [isDActive, setIsDActive] = useState(false);
+    const [isEnterActive, setIsEnterActive] = useState(false);
+    const [isSpaceActive, setIsSpaceActive] = useState(false);
+    useEffect(() => {
+        const switchCases = (key, toggle) => {
+            switch (key.toLowerCase()) {
+                case 'arrowdown':
+                    setIsSActive(toggle)
+                    break;
+                case 'keys':
+                    setIsSActive(toggle)
+                    break;
+                case 'arrowleft':
+                    setIsLeftArrowActive(toggle)
+                    break;
+                case 'keya':
+                    setIsLeftArrowActive(toggle)
+                    break;
+                case 'arrowup':
+                    setIsWActive(toggle)
+                    break;
+                case 'keyw':
+                    setIsWActive(toggle)
+                    break;
+                case 'arrowright':
+                    setIsDActive(toggle)
+                    break;
+                case 'keyd':
+                    setIsDActive(toggle)
+                    break;
+                case 'enter':
+                    setIsEnterActive(toggle)
+                    break;
+                case 'space':
+
+                    setIsSpaceActive(toggle)
+                    break;
+                default:
+                    break;
+              }
+        }
+        const handleKeyDown = (event) => {
+            
+            console.log(event);
+          let key = event.code || String.fromCharCode(event.keyCode || event.which);
+          switchCases(key, true);
+          if (key.toLowerCase() == 'space') {
+            // No permite el scroll por space pero no puedes dejar espacios en inputs.
+          }
+        }
+        const handleKeyUp = (event) => {
+          let key = event.code || String.fromCharCode(event.keyCode || event.which);
+          switchCases(key, false);
+        }
+    
+        // Agregar el evento 'keydown' al componente montado
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+        // Limpiar el evento al desmontar el componente
+        return () => {
+          document.removeEventListener('keydown', handleKeyDown);
+          document.removeEventListener('keyup', handleKeyUp);
+        };
+    }, []);
+    
+    return (
+        <div className="CuadroControles">
+            <div className="interior">
+                <div className='interiorEscenario'>
+                    <Personaje
+                        isAActive = { isAActive }
+                        isWActive = { isWActive }
+                        isDActive = { isDActive }
+                        isSActive = { isSActive }
+                        isEnterActive = { isEnterActive }
+                        isSpaceActive = { isSpaceActive }/>
+                </div>
+                <div className='interiorControles'>
+                    <Tecla 
+                        keyword = { 'A←' } 
+                        className = { 'AKeyword' } 
+                        isActive = { isAActive } 
+                        setActive = { setIsLeftArrowActive }/>
+                    <Tecla 
+                        keyword = { 'W↑' } 
+                        className = { 'WKeyword' } 
+                        isActive = { isWActive } 
+                        setActive = { setIsWActive }/>
+                    <Tecla 
+                        keyword = { 'D→' } 
+                        className = { 'DKeyword' } 
+                        isActive = { isDActive }  
+                        setActive = { setIsDActive }/>
+                    <Tecla 
+                        keyword = { '↵' } 
+                        className = { 'SKeyword' } 
+                        isActive = { isEnterActive }  
+                        setActive = { setIsEnterActive }/>
+                    <Tecla 
+                        keyword = { '⎵' }
+                        className = { 'UpArrowKeyword' } 
+                        isActive = { isSpaceActive }  
+                        setActive = { setIsSpaceActive }/>
+                    <Tecla
+                        keyword = { 'S↓' }
+                        className = { 'DownArrowKeyword' } 
+                        isActive = { isSActive }  
+                        setActive = { setIsSActive }/>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+export default CuadroControles;
---
+++ src/Components/Personaje.js
@@ -0,0 +1,124 @@
+import { useEffect, useState } from "react";
+import '../Styles/Personaje.css';
+const Personaje = ({ isAActive, isWActive, isDActive, isSActive, isEnterActive, isSpaceActive }) => {
+    const [ isJumping, setIsJumping ] = useState(false);
+    const [ isRunning, setIsRunning ] = useState(false);
+    const [ isShooting, setIsShooting] = useState(false);
+    const [ isDancing, setIsDancing] = useState(false);
+    const [ isStanding, setIsStanding] = useState(false);
+    const [ imagen, setImagen ] = useState('dancing.gif');
+    // Margin bottom
+    useEffect(() => {
+        const boxCharacterStyle = document.getElementById('borderBottomCharacter').style;
+        let toggle = true;
+      
+        const interval = setInterval(() => {
+          if (toggle) {
+            boxCharacterStyle.width = '50px';
+            toggle = false;
+          } else {
+            boxCharacterStyle.width = '100px';
+            toggle = true;
+          }
+        }, 700);
+      
+        return () => {
+          clearInterval(interval);
+        };
+    }, []);
+    
+    // Dirección
+    useEffect(()=>{
+        const characterElementStyle = document.getElementById('character').style;
+        if (isAActive) {
+            characterElementStyle.transform = "scaleX(-1)";
+        }
+        if (isDActive) {
+            characterElementStyle.transform = "scaleX(1)";
+        }
+    },[isAActive, isDActive]);
+
+    // Activación de eventos
+    useEffect(() => {
+        // Correr
+        const running = (isAActive || isDActive) && !isJumping ;
+        setIsRunning(running);
+      
+        // Saltar
+        let jumping = false;
+        if (!isJumping) {
+            jumping = isWActive;
+            setIsJumping(isWActive);
+            // Standing
+            const standing = !jumping && !running && !isShooting && !isDancing;
+            setIsStanding(standing);
+        }
+      
+        // Shooting
+        setIsShooting(isSpaceActive);
+      
+        // Dancing
+        const dancing = isEnterActive && !isJumping;
+        setIsDancing(dancing);
+
+      }, [isAActive, isWActive, isDActive, isEnterActive, isSpaceActive, isJumping, isShooting, isDancing]);
+
+    // Detección de eventos
+    useEffect(()=>{
+        // NOTAS
+        // No puede correr y balar
+        // Puede bailar y correr
+        // Puede saltar y disparar
+
+        if (isRunning) {
+            setImagen('runningRight.gif');
+        }
+        if (isStanding) {
+            setImagen('standRight.png');
+        }
+        if (isDancing) {
+            setImagen('dancing.gif');
+            if (isRunning) {
+                setImagen('runningRight.gif');
+            }
+        }
+        if (isShooting) {
+            setImagen('shootRight.png');
+            if (isJumping) {
+                setImagen('jumpShootRight.png');
+            }
+        } else {
+            if (isJumping) {
+                setImagen('jumpRight.png');
+            }
+        }
+    },[isJumping, isRunning, isShooting, isDancing, isStanding]);
+    
+    useEffect(()=>{
+        const characterElementStyle = document.getElementById('character').style;
+        if (isJumping) {
+            setImagen('jumpRight.png');
+            characterElementStyle.bottom = '60px';
+            setTimeout(() => {
+                characterElementStyle.bottom = '0px';
+            }, 400);
+            setTimeout(() => {
+                setIsJumping(false);
+            }, 800);
+        }
+    },[isJumping])
+
+
+    return (
+        <div className='ContainerPersonaje'>
+            <img id="character" src={ `/personaje/${imagen}` } alt="character"/>
+            <div className="borderBottom" id="borderBottomCharacter"></div>
+            {/* {isJumping && <div> isJumping </div>}
+            {isRunning && <div> isRunning </div>}
+            {isShooting && <div> isShooting </div>}
+            {isDancing && <div> isDancing </div>}
+            {isStanding && <div> isStanding </div>} */}
+        </div>
+    )
+}
+export default Personaje;
